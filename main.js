c950f2785e4244c16606dd664267e2a1c3f5c4e2
Ambar2010 <89517719+Ambar2010@users.noreply.github.com>
2022-06-29 21:12:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
+video = "";
+status = "";
+object = "";
+
+function preload() {
+    
+}
+
+
 function setup() {
-    canvas = createCanvas(480,400);
+    canvas = createCanvas(480,380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object = document.getElementById("input").value;
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.volume(1);
+    video.rate(1);
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
 }
